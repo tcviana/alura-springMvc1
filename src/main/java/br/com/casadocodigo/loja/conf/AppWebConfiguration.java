10ec53383939bcc64bbcb2beea65cb7953697972
tcviana <tcviana.sp@gmail.com>
2019-01-04 16:09:44 -0200

    Adionada função na finalização da compra para enviar dados do pagamento via JSon e retornar se a compra foi ou não aprovada.

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -65,6 +66,11 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+    
+    @Bean
+    private RestTemplate restTemplate(){
+    	return new RestTemplate();
+	}
 	
 }
 	
--- src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/PagamentoController.java
@@ -1,14 +1,19 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.concurrent.Callable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.models.CarrinhoCompras;
+import br.com.casadocodigo.loja.models.DadosPagamento;
 
 @Controller
 @RequestMapping("/pagamento")
@@ -18,13 +23,26 @@ public class PagamentoController {
 	@Autowired
 	private CarrinhoCompras carrinho;
 	
-	@RequestMapping("/finalizar")
-	public ModelAndView finalizar(RedirectAttributes model) {
-		System.out.println(carrinho.getTotal());
-		ModelAndView modelAndView = new ModelAndView("redirect:/produtos");
-		model.addFlashAttribute("sucesso","Compra realizada com sucesso.");
-		
-		return modelAndView;
-		
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@RequestMapping(value="/finalizar", method=RequestMethod.POST)
+	public Callable<ModelAndView> finalizar(RedirectAttributes model) {
+		return() -> {
+			try {
+				String response = 
+						restTemplate.postForObject("http://book-payment.herokuapp.com/payment", 
+								new DadosPagamento(carrinho.getTotal()), String.class);
+				System.out.println(response);
+				model.addFlashAttribute("sucesso",response);		
+				return new ModelAndView("redirect:/produtos");
+			}catch (Exception e) {
+				e.printStackTrace();
+				model.addFlashAttribute("falha","Valor maior que o permitido.");
+				return new ModelAndView("redirect:/produtos");
+			}
+			
+			
+		};		
 	}
 }
